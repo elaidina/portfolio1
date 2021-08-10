7b47736982e8db008ac698bbfadc376ef118f689
Laura Horackova <39065215+elaidina@users.noreply.github.com>
2021-08-10 21:33:13 +0200

    init

---
+++ app.js
@@ -0,0 +1,6 @@
+const notes = document.getElementById("notes");
+const addNote = document.getElementById("addNote");
+const darkBtn = document.getElementById("darkBtn");
+
+const time = new Date();
+console.log(time);
