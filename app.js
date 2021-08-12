0046567c2e02fd4f8cb73834132fed44247e05b2
Laura Horackova <39065215+elaidina@users.noreply.github.com>
2021-08-12 17:18:11 +0200

    add note

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const notes = document.getElementById("notes");
-const addNote = document.getElementById("addNote");
 const darkBtn = document.getElementById("darkBtn");
 const darkPic = document.querySelector(".dark");
 const navbar = document.querySelector("Nav");
 const timeEl = document.querySelector(".time");
 
+const notes = document.getElementById("notes");
+const addNote = document.getElementById("addNote");
+const form = document.querySelector(".notesForm");
+let newNote = document.getElementById("newNote");
+// console.log(newNote);
+if (form) {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let note = event.target[0].value;
+
+    if (note) {
+      const noteLi = document.createElement("li");
+      // noteLi.classList.add("")
+      noteLi.innerHTML = note;
+      notes.appendChild(noteLi);
+      event.target[0].value = "";
+    }
+  });
+}
+
 setInterval(() => {
   const time = new Date();
-  console.log(time);
+  // console.log(time);
   if (timeEl) {
     timeEl.innerText = time;
   }
